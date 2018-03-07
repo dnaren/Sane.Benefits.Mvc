c6e82c50ae625e9c43f3d7bb244d5355c3e8fcbd
Narendiran Dorairaj <Narendiran.Dorairaj@benefitmall.com>
2018-03-06 19:42:57 -0600

    Added unit test project for MVC

---
+++ tests/Sane.Benefits.Mvc.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sane.Benefits.Mvc.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sane.Benefits.Mvc.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("27758ec7-454a-41bb-809b-9e70b7439aea")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
